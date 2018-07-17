3d8473989236d4935c481cbbb345f387ee91a043
HalfBloodScot <13808746+HalfBloodScot@users.noreply.github.com>
2018-07-18 11:34:29 +1200

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -1,1 +1,14 @@
 #include "BST.h"
+
+bool BST::contains(const T& obj) {
+    if (value == obj) {
+        return true; 
+    }
+    if (left && value > obj) {
+        return  left->(contains(obj));
+    }
+    if (right && value < obj) {
+        return right->contains(obj)); 
+    }
+    return false; 
+}
